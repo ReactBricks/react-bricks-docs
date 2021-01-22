4b44ec478369ecdcf81cdaf4c2e9da36219a72bf
Matteo Frana <f2@f2.net>
2021-01-22 13:54:51 +0100

    Algolia DocSearch

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
+            async
+            defer
+          />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
